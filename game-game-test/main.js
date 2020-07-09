facbbda1d250c99aac1077392b2b1a465928db1c
Сергей Соломко <serj-solomko@mail.ru>
2020-07-09 21:37:59 +0300

    Update main.js

--- game-game-test/main.js
+++ game-game-test/main.js
@@ -17,7 +17,7 @@ $(document).ready(function(){
    if (checkIfFlashEnabled()) {
       //
    } else {
-      var html="<div class='trFlash' style='background-image: url('Game-Game_files/image.jpg');'><div class='blur'></div><div class='trFlash__main'><div class='trFlash__content'><p>Для запуска игры необходимо активировать Flash-плеер</p><a class='trFlash__btn' href='https://get.adobe.com/flashplayer/' target='_blank' rel='nofollow'>активировать</a><span>или</span><ol><li>Нажмите на иконку&nbsp;<i>i</i>&nbsp;вверху слева в адресной строке браузера</li><li>В выпадающем списке 'Flash' выберите пункт 'разрешить'</li><li>Обновите страницу и наслаждайтесь игрой</li></ol></div></div></div>";
+      var html="<div class='trFlash' style='background-image: url('Game-Game_files/image.jpg');'><div class='blur'></div><div class='trFlash__main'><div class='trFlash__content'><p>Для запуска игры необходимо активировать Flash-плеер</p><a class='trFlash__btn' href='https://get.adobe.com/flashplayer/' target='_blank' rel='nofollow'>активировать</a><span>или</span><ol><li>Нажмите на иконку&nbsp;<i>i</i>&nbsp;вверху слева в адресной строке браузера</li><li>В выпадающем списке 'Flash' выберите пункт 'Разрешить'</li><li>Обновите страницу и наслаждайтесь игрой</li></ol></div></div></div>";
       $('.game-src').after(html);
    }
 })
