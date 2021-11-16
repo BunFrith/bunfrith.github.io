e549c83473a1a6e0a99cf57df792e270a11e2e0d
BunFrith <bunfrith.dev@gmail.com>
2021-11-16 23:48:25 +0200

    yayoye preview template final version

---
+++ precache-manifest.ff00fd378b952aab638151ad19afff13.js
@@ -0,0 +1,110 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "aa3ebf98a3288dfadf58",
+    "url": "/css/app.b6942972.css"
+  },
+  {
+    "revision": "a91a3afab444458f9cf1",
+    "url": "/css/chunk-06628ed0.c42435d9.css"
+  },
+  {
+    "revision": "dbf1c97db9cdeedf0945",
+    "url": "/css/chunk-320c502f.f4b19030.css"
+  },
+  {
+    "revision": "73eecb8f89b9d01d1aa9",
+    "url": "/css/chunk-b84039b2.b89169b9.css"
+  },
+  {
+    "revision": "f1b559848ca3c8bd83af",
+    "url": "/css/chunk-vendors.8fe7522e.css"
+  },
+  {
+    "revision": "322b87cc74acf1f88fcc7c6b7393b5eb",
+    "url": "/img/dislike.322b87cc.svg"
+  },
+  {
+    "revision": "1911fea2d91b4f61d2844c714345c946",
+    "url": "/img/fullscreen.1911fea2.svg"
+  },
+  {
+    "revision": "97a52ca9023f6b29f4e034b864e30360",
+    "url": "/img/game-bg.97a52ca9.jpg"
+  },
+  {
+    "revision": "755d10203e08607fcc4d372e0ef566d4",
+    "url": "/img/game-item.755d1020.png"
+  },
+  {
+    "revision": "7b805f39c0ec25880ad3433659faa678",
+    "url": "/img/like-small.7b805f39.svg"
+  },
+  {
+    "revision": "e9a98a1d29cdcd9e0f0d1b14c10032ca",
+    "url": "/img/like.e9a98a1d.svg"
+  },
+  {
+    "revision": "8a97778ebc041a9281631086e70b46b1",
+    "url": "/img/logo.8a97778e.svg"
+  },
+  {
+    "revision": "6cbfbc09374208085fd0bb55379f3845",
+    "url": "/img/pwa-icon.6cbfbc09.svg"
+  },
+  {
+    "revision": "d7a38a589317c80d770d699b19f5d819",
+    "url": "/img/search.d7a38a58.svg"
+  },
+  {
+    "revision": "cb172235d3e2f90edaa01757a615c4c3",
+    "url": "/img/small-pwa.cb172235.svg"
+  },
+  {
+    "revision": "749c41c8b0591a80acce35a0da7f59dd",
+    "url": "/img/star-small.749c41c8.svg"
+  },
+  {
+    "revision": "8fff700f66dc566c4063423cbc4d73da",
+    "url": "/img/star.8fff700f.svg"
+  },
+  {
+    "revision": "f1451b0ad94eadf09d69833f19524596",
+    "url": "/img/up-btn.f1451b0a.svg"
+  },
+  {
+    "revision": "8133e5fc5208086a9609927464d5b07e",
+    "url": "/index.html"
+  },
+  {
+    "revision": "aa3ebf98a3288dfadf58",
+    "url": "/js/app.161cb22d.js"
+  },
+  {
+    "revision": "a91a3afab444458f9cf1",
+    "url": "/js/chunk-06628ed0.249048f4.js"
+  },
+  {
+    "revision": "dbf1c97db9cdeedf0945",
+    "url": "/js/chunk-320c502f.cd0e9a47.js"
+  },
+  {
+    "revision": "73eecb8f89b9d01d1aa9",
+    "url": "/js/chunk-b84039b2.5ace2d0c.js"
+  },
+  {
+    "revision": "f1b559848ca3c8bd83af",
+    "url": "/js/chunk-vendors.006139fc.js"
+  },
+  {
+    "revision": "922366607c5a3cda943e30df1a1b8abd",
+    "url": "/manifest.json"
+  },
+  {
+    "revision": "b6216d61c03e6ce0c9aea6ca7808f7ca",
+    "url": "/robots.txt"
+  },
+  {
+    "revision": "228517a125d5370a7d3966717c49808a",
+    "url": "/yayoye-icon.ico"
+  }
+]);
